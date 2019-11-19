2146c7bdfa4c2db9947d86df22183280dff93b16
Nikolay.Petrusenko <saltpan@gmail.com>
2019-11-19 13:10:50 +0300

    fix

--- src/main/java/HelloAction.java
+++ src/main/java/HelloAction.java
@@ -4,56 +4,56 @@ import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.util.Pair;
 import com.intellij.psi.*;
+import com.intellij.psi.javadoc.PsiDocComment;
 import com.intellij.psi.util.PsiTreeUtil;
 
 public class HelloAction extends AnAction {
+    Project project = null;
+
     public HelloAction() {
         super("Hello");
     }
 
     public void actionPerformed(AnActionEvent anActionEvent) {
-        Project project = anActionEvent.getProject();
-        Messages.showMessageDialog(project, "Hello world!", "Greeting", Messages.getInformationIcon());
+        project = anActionEvent.getProject();
 
         Editor editor = anActionEvent.getData(CommonDataKeys.EDITOR);
         PsiFile psiFile = anActionEvent.getData(CommonDataKeys.PSI_FILE);
         if (editor == null || psiFile == null) return;
         int offset = editor.getCaretModel().getOffset();
 
-        final StringBuilder infoBuilder = new StringBuilder();
         PsiElement element = psiFile.findElementAt(offset);
-        infoBuilder.append("Element at caret: ").append(element).append("\n");
-        if (element != null) {
-            PsiMethod containingMethod = PsiTreeUtil.getParentOfType(element, PsiMethod.class);
-            infoBuilder
-                    .append("Containing method: ")
-                    .append(containingMethod != null ? containingMethod.getName() : "none")
-                    .append("\n");
-            if (containingMethod != null) {
-                PsiClass containingClass = containingMethod.getContainingClass();
-                infoBuilder
-                        .append("Containing class: ")
-                        .append(containingClass != null ? containingClass.getName() : "none")
-                        .append("\n");
-
-                infoBuilder.append("Local variables:\n");
-                containingMethod.accept(new JavaRecursiveElementVisitor() {
-                    @Override
-                    public void visitLocalVariable(PsiLocalVariable variable) {
-                        super.visitLocalVariable(variable);
-                        infoBuilder.append(variable.getName()).append("\n");
-                    }
-                });
-            }
+        if (element == null) {
+            return;
         }
-        Messages.showMessageDialog(anActionEvent.getProject(), infoBuilder.toString(), "PSI Info", null);
+
+        PsiMethod containingMethod = PsiTreeUtil.getParentOfType(element, PsiMethod.class);
+        if (containingMethod == null) {
+            return;
+        }
+
+        PsiClass containingClass = containingMethod.getContainingClass();
+
+        PsiClass[] interfaces = containingClass.getInterfaces();
+
+        if (interfaces.length != 1) {
+            log("More than 1 interface");
+            return;
+        }
+
+        PsiClass anInterface = interfaces[0];
+        for (PsiMethod intMethod : anInterface.getAllMethods()) {
+            PsiDocComment docComment = intMethod.getDocComment();
+
+            log("method - " + intMethod.toString());
+            log("docComment - " + docComment.toString());
+        }
     }
 
-    @Override
-    public void update(AnActionEvent e) {
-        Editor editor = e.getData(CommonDataKeys.EDITOR);
-        PsiFile psiFile = e.getData(CommonDataKeys.PSI_FILE);
-        e.getPresentation().setEnabled(editor != null && psiFile != null);
+
+    public void log(String text) {
+        Messages.showMessageDialog(project, text, "Logger", null);
     }
 }
