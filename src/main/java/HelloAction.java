4bbbc123311ad2b1e87c931bdf9c3ffca3d5d88a
Nikolay.Petrusenko <saltpan@gmail.com>
2019-11-20 15:45:46 +0300

    works

--- src/main/java/HelloAction.java
+++ src/main/java/HelloAction.java
@@ -1,20 +1,27 @@
+import com.intellij.ide.SaveAndSyncHandler;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.command.WriteCommandAction;
-import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ex.ProjectManagerEx;
+import com.intellij.openapi.vfs.VirtualFileManager;
 import com.intellij.psi.*;
 import com.intellij.psi.util.PsiTreeUtil;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 public class HelloAction extends AnAction {
@@ -96,8 +103,35 @@ public class HelloAction extends AnAction {
         PsiDirectory parent = anInterface.getContainingFile().getParent();
 
         deleteFile(anInterface);
-        moveFile(containingClass, parent);
-        renameFile(containingClass, interfaceName);
+
+        project.save();
+        FileDocumentManager.getInstance().saveAllDocuments();
+        ProjectManagerEx.getInstanceEx().blockReloadingProjectOnExternalChanges();
+
+
+        String classText = containingClass.getContainingFile().getText();
+        String[] split = classText.split("\\n");
+        String newText = Stream.of(split)
+                .filter(line -> !line.contains("@Override"))
+                .map(line -> line.replaceAll("default ", "public "))
+                .collect(joining("\n"));
+
+        Path path = Paths.get(containingClass.getContainingFile().getVirtualFile().getPath());
+
+        try {
+            Files.write(path, newText.getBytes());
+        } catch (IOException e) {
+            log(e.getMessage());
+        }
+
+        try {
+            Thread.sleep(500L);
+            SaveAndSyncHandler.getInstance().refreshOpenFiles();
+            VirtualFileManager.getInstance().refreshWithoutFileWatcher(false);
+            ProjectManagerEx.getInstanceEx().unblockReloadingProjectOnExternalChanges();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     private void copyMethod(PsiMethod intMethod, PsiMethod previousClassMethod) {
